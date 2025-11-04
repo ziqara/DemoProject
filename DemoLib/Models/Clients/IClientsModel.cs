b45b932019c75c5cb87147fe17903f5d0fb06b54
ziqara <165387888+ziqara@users.noreply.github.com>
2025-11-04 19:24:32 +0300

    Интерфейс для работы с клиентами, и его реализация интерфейс

---
+++ DemoLib/Models/Clients/IClientsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLib.DataModel.Clients;
+
+namespace DemoLib.Models.Clients
+{
+    public interface IClientsModel
+    {
+        List<Client> ReadAllClients();
+
+        int GetClientsCount();
+    }
+}
---
+++ DemoLib/Models/Clients/MySQLClientsModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLib.DataModel.Clients;
+using DemoLib.DataModel.Orders;
+using MySql.Data.MySqlClient;
+
+namespace DemoLib.Models.Clients
+{
+    public class MySQLClientsModel : IClientsModel
+    {
+        private const string connStr = "server=localhost;user=root;database=clients_db;password=1234567;port=3307;";
+        public int GetClientsCount()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT COUNT(id) FROM clientsinfo";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    int result = Convert.ToInt32(command.ExecuteScalar().ToString());
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Client> ReadAllClients()
+        {
+            List<Client> clients = new List<Client>();
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT id, clientName, phone, mail, description, imagePath FROM clientsinfo";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Client client = new Client(reader.GetInt32(0));
+
+                            client.Name = reader.GetString(1);
+                            client.Phone = reader.GetString(2);
+                            client.Mail = reader.GetString(3);
+                            client.Description = reader.GetString(4);
+                            client.ImagePath = reader.GetString(5);
+
+                            clients.Add(client);
+                        }
+                    }
+
+                    foreach (Client client in clients)
+                    {
+                        string orderQuery = "SELECT article, date, price, count FROM orders WHERE idclient = " + client.ID;
+                        MySqlCommand orderCommand = new MySqlCommand(orderQuery, connection);
+                        using (MySqlDataReader orderReader = orderCommand.ExecuteReader())
+                        {
+                            while (orderReader.Read())
+                            {
+                                OrderRecord orderRecord = new OrderRecord();
+                                orderRecord.NameProduct = orderReader.GetString(0);
+                                orderRecord.SaleDate = orderReader.GetDateTime(1);
+                                orderRecord.Price = orderReader.GetDouble(2);
+                                orderRecord.Count = orderReader.GetInt32(3);
+
+                                client.order.AddRecord(orderRecord);
+                            }
+                        }
+                    }
+
+                    return clients;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
