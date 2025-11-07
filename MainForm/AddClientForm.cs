2159d0bb779c004ebaf15f33c0624bb984be9cd0
ziqara <165387888+ziqara@users.noreply.github.com>
2025-11-07 19:16:49 +0300

    Инициализировал поля

--- MainForm/AddClientForm.cs
+++ MainForm/AddClientForm.cs
@@ -4,17 +4,28 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DemoLib.DataModel.Clients;
+using DemoLib.Models.Clients;
 
 namespace MainForm
 {
     public partial class AddClientForm : Form
     {
-        public AddClientForm()
+        private Client newClient_;
+        private string selectedImagePath_;
+        private MySQLClientsModel model_;
+        private int type_;
+
+        public AddClientForm(MySQLClientsModel model, int type, Client client)
         {
             InitializeComponent();
+            model_ = model;
+            type_ = type;
+            newClient_ = client;
         }
     }
 }
